b50b396a3976806e7000c09c6cda452d9c010306
Your Name <mutalovnuriddin651@gmail.com>
2025-02-27 18:20:21 +0500

    frontend qismi yozildi va backend bilan url lar ulandi

--- products/views.py
+++ products/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
 
+from .models import Category, Advertisement, User
+from django.views.generic import ListView, CreateView, DetailView
+
 # Create your views here.
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'index.html'
+
+
+class AdvertisementCreateView(CreateView):
+    model = Advertisement
+    template_name = 'announce.html'
+
+
+class AdvertisementListView(ListView):
+    model = Advertisement
+    template_name = 'main.html'
+    context_object_name = 'advertisements'
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id')
+        if category_id:
+            return Advertisement.objects.filter(category_id=category_id)
+        return Advertisement.objects.all()
+
+class AdvertisementDetailView(DetailView):
+    model = Advertisement
+    template_name = 'single.html'
+    pk_url_kwarg = 'id'
+
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+class UserProfileCreateView(CreateView):
+    model = User
+    template_name = 'profile.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
