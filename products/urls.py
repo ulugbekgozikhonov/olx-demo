94b53156d8f8950153f021c0f964c9bcff0b265a
Ulug'bek G'ozixonov <ulugbekgozixonov@gmail.com>
2025-03-01 18:52:46 +0500

    confilictlarni marge qilish jarayonin in oxiriga yetqazi kerka

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import CategoryListView, login, AdvertisementCreateView, UserProfileCreateView, AdvertisementDetailView, AdvertisementListView, AdvertisementSearchView
+from .views import CategoryListView, AdvertisementCreateView, UserProfileCreateView, AdvertisementDetailView, AdvertisementListView, AdvertisementSearchView
 
 urlpatterns = [
     path('', CategoryListView.as_view(), name='home'),
